a4f11bb0eeab506bf039dcc6556f270b69628c73
Ana Pereira <belenp2000@gmail.com>
2025-07-13 19:18:01 -0400

    Delete practice_1 directory

--- practice_1/TESS_LC/TESS_CSV_FILES/find_tess.sh
+++
@@ -1,6 +0,0 @@
-# This code searches for all the .csv files in the current folder, saves their names
-# in a file named tess_csv.txt and then prints that list to the screen.
-tess_names = " tess_csv.txt "
-ls *. csv > " $tess_names "
-echo " Files found $tess_names : "
-cat $tess_names
